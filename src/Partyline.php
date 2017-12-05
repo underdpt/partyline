c97da4e2d8b42930ff13cf74b81a87aa3768ef67
underdpt <david@mainsip.com>
2017-12-05 18:31:51 +0100

    Enables sending the output to Laravel's Log

--- src/Partyline.php
+++ src/Partyline.php
@@ -3,6 +3,7 @@
 namespace Wilderborn\Partyline;
 
 use Illuminate\Console\Command;
+use Log;
 
 class Partyline
 {
@@ -10,6 +11,11 @@ class Partyline
      * @var Command|NullConsole
      */
     private $console;
+
+    /**
+     * @var bool Send lines to Laravel's log
+     */
+    private $log = false;
 
     /**
      * Set the console command instance
@@ -31,8 +37,60 @@ class Partyline
      */
     public function __call($name, $arguments)
     {
+        if ($this->log) {
+            $this->sendToLog($name, $arguments[0]);
+        }
+
         $console = $this->console ?: new NullConsole;
 
         return call_user_func_array([$console, $name], $arguments);
+    }
+
+    /**
+     * Enables or disables logging all the lines to Laravel's Log
+     * @param bool $log
+     */
+    public function setEnableLog(bool $log)
+    {
+        $this->log = $log;
+    }
+
+    /**
+     * Is log enabled?
+     * @return bool
+     */
+    public function getEnableLog()
+    {
+        return $this->log;
+    }
+
+    /**
+     * Logs $string to Laravel's Log, mapping the console function being called
+     * to a suitable log level
+     * @param  string $name   Command function called
+     * @param  string $string Text to log
+     * @return void
+     */
+    public function sendToLog($name, $string)
+    {
+        switch ($name) {
+            case 'info':
+                Log::info($string);
+                break;
+            case 'error':
+                Log::error($string);
+                break;
+            case 'warn':
+                Log::warning($string);
+                break;
+            case 'alert':
+                Log::notice($string);
+                break;
+            case 'question':
+            case 'comment':
+            case 'line':
+            default:
+                Log::debug($string);
+        }
     }
 }
